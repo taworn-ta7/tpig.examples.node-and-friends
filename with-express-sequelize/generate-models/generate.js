6eb311dca68af6b1e544a29b45df2fc6384f9e4c
Taworn Ta <taworn.ta7@gmail.com>
2021-01-30 17:00:45 +0700

    - redirect output to files

--- with-express-sequelize/generate-models/generate.js
+++ with-express-sequelize/generate-models/generate.js
@@ -1,24 +1,22 @@
 'use strict'
+const fs = require('fs')
 const models = require('../models')
-const { JsonSchemaManager, JsonSchema7Strategy, OpenApi3Strategy } = require('@alt3/sequelize-to-json-schemas')
+const { JsonSchemaManager, JsonSchema7Strategy } = require('@alt3/sequelize-to-json-schemas')
 const schemaManager = new JsonSchemaManager({
     baseUri: '/with-express-sequelize/',
     absolutePaths: true,
     secureSchemaUri: true
-});
+})
+
+// save function
+const save = (manager, strategy, model, file) => {
+    const schema = manager.generate(model, strategy)
+    const text = JSON.stringify(schema, null, 4)
+    fs.writeFileSync(file, text)
+}
 
 // now generate a JSON Schema Draft-07 model schema
 const jsonSchema7Strategy = new JsonSchema7Strategy()
-let schema0
-console.log('Schema #0')
-schema0 = schemaManager.generate(models.Users, jsonSchema7Strategy)
-console.log(JSON.stringify(schema0, null, 4))
-schema0 = schemaManager.generate(models.Profiles, jsonSchema7Strategy)
-console.log(JSON.stringify(schema0, null, 4))
-schema0 = schemaManager.generate(models.Items, jsonSchema7Strategy)
-console.log(JSON.stringify(schema0, null, 4))
-
-// and/or the OpenAPI 3.0 equivalent
-const schema1 = schemaManager.generate(models.Users, new OpenApi3Strategy())
-console.log('Schema #1')
-console.log(JSON.stringify(schema1, null, 4))
+save(schemaManager, jsonSchema7Strategy, models.Users, 'output/User.json')
+save(schemaManager, jsonSchema7Strategy, models.Profiles, 'output/Profiles.json')
+save(schemaManager, jsonSchema7Strategy, models.Items, 'output/Items.json')
