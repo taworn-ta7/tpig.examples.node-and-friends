dc7db60cf6d9598bda7c6386350c687c3a7f108d
Taworn Ta <taworn@digio.co.th>
2021-02-02 09:54:06 +0700

    - more concise in schema writing - more tests

--- with-sequelize/tests/Users.test.js
+++ with-sequelize/tests/Users.test.js
@@ -13,7 +13,7 @@ describe("testing Users schema", () => {
         expect(result).toBe(true)
     })
 
-    test("create: ok with sub-profiles", () => {
+    test("create: ok with children", () => {
         const json = {
             id: 0,
             username: 'john',
@@ -47,10 +47,44 @@ describe("testing Users schema", () => {
         expect(result).toBe(false)
     })
 
+    test("create: failed because mistype username", () => {
+        const json = {
+            id: 0,
+            usernam_: 'john',
+            password: 'password'
+        }
+        const result = schemas.Users(json)
+        expect(result).toBe(false)
+    })
+
     test("create: failed because no password", () => {
         const json = {
             id: 0,
             username: 'john'
+        }
+        const result = schemas.Users(json)
+        expect(result).toBe(false)
+    })
+
+    test("create: failed with children", () => {
+        const json = {
+            id: 0,
+            username: 'john',
+            password: 'password',
+            profiles: [
+                {
+                    id: 0,
+                    uid: 0,
+                    name: 'John Doe',
+                    items: [
+                        {
+                            id: 0,
+                            uid: 0,
+                            name: ''
+                        }
+                    ]
+                }
+            ]
         }
         const result = schemas.Users(json)
         expect(result).toBe(false)
@@ -81,6 +115,24 @@ describe("testing Users schema", () => {
         }
         const result = schemas.Users(json)
         expect(result).toBe(true)
+    })
+
+    test("update: failed because username is less than minLength", () => {
+        const json = {
+            id: 1,
+            username: 'jo'
+        }
+        const result = schemas.Users(json)
+        expect(result).toBe(false)
+    })
+
+    test("unknown: failed because no id", () => {
+        const json = {
+            username: 'john',
+            username: 'password'
+        }
+        const result = schemas.Users(json)
+        expect(result).toBe(false)
     })
 
 })
