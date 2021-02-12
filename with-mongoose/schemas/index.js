d55928e0a40e7c69a7ef708de82126c8d75bbdbd
Taworn Ta <taworn.ta7@gmail.com>
2021-02-12 19:59:46 +0700

    - add schemas and tests for with-mongoose

---
+++ with-mongoose/schemas/index.js
@@ -0,0 +1,17 @@
+'use strict'
+const Ajv = require('ajv').default
+const ajv = new Ajv({
+    metaSchema: require('ajv/lib/refs/json-schema-draft-07.json'),
+    schemas: [
+        require('./Users.json')
+    ]
+})
+
+const addFormats = require('ajv-formats')
+addFormats(ajv)
+
+const baseUri = 'https://node-and-friends/with-mongoose/'
+module.exports = {
+    createUser: ajv.compile({ '$ref': `${baseUri}Users.json#/definitions/createUser` }),
+    updateUser: ajv.compile({ '$ref': `${baseUri}Users.json#/definitions/updateUser` })
+}
