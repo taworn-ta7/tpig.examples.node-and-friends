70da2d3d1bfcbe21f31eb4378a27ed5726981050
Taworn Ta <taworn.ta7@gmail.com>
2021-02-16 17:28:18 +0700

    - organize code

--- with-express-sequelize-authen-tests/schemas/index.js
+++ with-express-sequelize-authen-tests/schemas/index.js
@@ -3,7 +3,6 @@ const Ajv = require('ajv').default
 const ajv = new Ajv({
     metaSchema: require('ajv/lib/refs/json-schema-draft-07.json'),
     schemas: [
-        require('./Users.json'),
         require('./Profiles.json'),
         require('./Items.json')
     ],
@@ -15,9 +14,6 @@ addFormats(ajv)
 
 const baseUri = 'https://node-and-friends/with-express-sequelize-authen-tests/'
 module.exports = {
-    createUser: ajv.compile({ '$ref': `${baseUri}Users.json#/definitions/createUser` }),
-    updateUser: ajv.compile({ '$ref': `${baseUri}Users.json#/definitions/updateUser` }),
-    updateUserPassword: ajv.compile({ '$ref': `${baseUri}Users.json#/definitions/updateUserPassword` }),
     createProfile: ajv.compile({ '$ref': `${baseUri}Profiles.json#/definitions/createProfile` }),
     updateProfile: ajv.compile({ '$ref': `${baseUri}Profiles.json#/definitions/updateProfile` }),
     createItem: ajv.compile({ '$ref': `${baseUri}Items.json#/definitions/createItem` }),
--- with-express-sequelize-authen-tests/scripts/insert_test_data.js
+++ with-express-sequelize-authen-tests/scripts/insert_test_data.js
@@ -8,41 +8,6 @@ const run = async () => {
     try {
         await db.sync({ force: true })
 
-        let password = authen.setPassword('admin//pass')
-        await models.Users.create({
-            username: 'admin',
-            displayName: 'Administrator',
-            role: 'admin',
-            salt: password.salt,
-            hash: password.hash
-        })
-
-        password = authen.setPassword('user0//pass')
-        await models.Users.create({
-            username: 'user0',
-            displayName: 'User 0',
-            role: 'user',
-            salt: password.salt,
-            hash: password.hash
-        })
-
-        password = authen.setPassword('user1//pass')
-        await models.Users.create({
-            username: 'user1',
-            displayName: 'User 1',
-            role: 'user',
-            salt: password.salt,
-            hash: password.hash
-        })
-
-        password = authen.setPassword('user2//pass')
-        await models.Users.create({
-            username: 'user2',
-            displayName: 'User 2',
-            role: 'user',
-            salt: password.salt,
-            hash: password.hash
-        })
     }
     catch (ex) {
         logger.error(ex)
